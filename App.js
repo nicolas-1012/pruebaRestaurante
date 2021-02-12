42f739a6eb66cc0a6f4ae600255bf11ccac03105
Sergio Celemin <sergiocelemin1998@gmail.com>
2021-02-12 18:01:59 -0500

    Cambiar texto hola

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.tituloTexto}>Menu</Text>
+      <Text style={styles.tituloTexto}>Menu Hola!</Text>
       <StatusBar style="auto" />
     </View>
   );
